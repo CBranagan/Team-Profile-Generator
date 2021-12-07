8523cad0a404a61bcd0de9bcc3f57a31be5ffa2f
Chad Branagan <chad.branagan@gmail.com>
2021-12-06 21:30:10 -0800

    finished getting the for loop to work and finished styling

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -3,64 +3,72 @@
 
 function employeeCards(empList) {  
     
+    var cardArray=[];
 
-    
-    for(var i = 0; i < empList.length; i++)
-
+    for(var i = 0; i < empList.length; i++) {
+        
+        
         if(empList[i].role === 'Manager') {
 
-      return `
+      cardArray.push( `
 
-        <div class="card" style= "width: 18rem;">
-            <div class="card-header">
-                <h4>${empList[i].name}<h4>
-                <h5>${empList[i].role}<h5>
+        <div class="col-3">
+            <div class="card">
+            <div class="card-header bg-primary">
+                <h4 class="text-light">${empList[i].name}<h4>
+                <h5 class="text-light">${empList[i].role}<h5>
             </div>
             <div class="card-body">Emp ID:${empList[i].id}                  
             </div>
-            <div class="card-body">Email:${empList[i].email}
-            </div>
+            <a href="mailto:${empList[i].email} class="card-body">Email: ${empList[i].email}</a>
+            
             <div class="card-body">Office Number:${empList[i].office}
             </div>
+            </div>
         </div>             
-        `
+        `)
         }
-          if(empList[i].role === 'Engineer') {
+          if (empList[i].role === 'Engineer') {
             
-        return `
+            cardArray.push( `
 
-        <div class="card" style= "width: 18rem;">
-            <div class="card-header">
-                <h4>${empList[i].name}<h4>
-                <h5>${empList[i].role}<h5>
+        <div class="col-3">
+        <div class="card">
+            <div class="card-header bg-primary">
+                <h4 class="text-light">${empList[i].name}<h4>
+                <h5 class="text-light">${empList[i].role}<h5>
             </div>
             <div class="card-body">Emp ID:${empList[i].id}                  
             </div>
-            <div class="card-body">Email:${empList[i].email}
-            </div>
-            <a href='${empList[i].github}.github.com' class="card-body">GitHub${empList[i].github}</a>
-        </div>             
-        `
-        } else {
-
-         return `
-
-        <div class="card" style= "width: 18rem;">
-            <div class="card-header">
-                <h4>${empList[i].name}<h4>
-                <h5>${empList[i].role}<h5>
-            </div>
-            <div class="card-body">Emp ID:${empList[i].id}                  
-            </div>
-            <div class="card-body">Email:${empList[i].email}
-            </div>
-            <div class="card-body">School:${empList[i].school}
-        </div>             
-        `
-
-        } 
-
+            <a href="mailto:${empList[i].email} class="card-body">Email: ${empList[i].email}</a>
             
+            <a href='https://github.com/${empList[i].github}' class="card-body">GitHub ${empList[i].github}</a>
+        </div>  
+        </div>           
+        `)
+        }if (empList[i].role === "Intern") {
+
+            cardArray.push( `
+
+        <div class="col-3">
+        <div class="card">
+            <div class="card-header bg-primary">
+                <h4 class="text-light">${empList[i].name}<h4>
+                <h5 class="text-light">${empList[i].role}<h5>
+            </div>
+            <div class="card-body">Emp ID: ${empList[i].id}                  
+            </div>
+            <a href="mailto:${empList[i].email} class="card-body">Email: ${empList[i].email}</a>
+            
+            <div class="card-body">School: ${empList[i].school}
+            </div>
+        </div>
+        </div>             
+        `)
+
+        } }
+
+         return cardArray.join(''); 
 
     }
 
@@ -72,7 +80,7 @@ function employeeCards(empList) {
 generateMarkdown = function(empList) {
 
 
-    return `
+    return`
     
     <!DOCTYPE html>
     <html lang="en">
@@ -86,8 +94,18 @@ generateMarkdown = function(empList) {
     </head>
     <body>
 
-    ${employeeCards(empList)}
     
+    <nav class="navbar navbar-light bg-danger">
+    <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1 text-center text-light">My Team</span>
+    </div>
+    </nav>
+    <div class="container">
+    <div class="row">
+
+    ${employeeCards(empList)}
+    </div>
+    </div>
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
